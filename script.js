9cfc0808a75ead02f1188f4c4cabe359d38ac39a
Joseph Angelo Enciso <jangeloenciso@gmail.com>
2022-03-09 16:50:50 +0800

    bug fixes and notes

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function getNum() {
     var num = document.querySelector(
-        '#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.DAQYgc.xPh1xb.P9KVBf > div.MaVIwe > div.Ok4Bg > div > div > div:nth-child(2) > div > div'
-    ).textContent;
+        '#ow4 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.DAQYgc.xPh1xb.P9KVBf > div.MaVIwe > div.Ok4Bg > div > div > div:nth-child(2) > div > div'
+    );
 
-    return num;
+    return num.textContent;
 }
 
 function leaveCall() {
     var exit = document.querySelector(
-        '#ow3 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.DAQYgc.xPh1xb.P9KVBf > div.rceXCe > div > div.NHaLPe.CoOyx > span > button'
+        '#ow4 > div.T4LgNb > div > div:nth-child(9) > div.crqnQb > div.DAQYgc.xPh1xb.P9KVBf > div.rceXCe > div > div.NHaLPe.CoOyx > span > button'
     );
 
     exit.click();
@@ -29,15 +29,15 @@ function checkPage() {
     return false;
 }
 
+// checkPage() function not working for some reason
+
 function main() {
     setInterval(function () {
         console.log('Yeet Meet running!');
 
-        if (checkPage() == true) {
-            if (getNum() <= 4) {
-                console.log('Leaving call.');
-                leaveCall();
-            }
+        if (getNum() <= 4) {
+            console.log('Leaving call.');
+            leaveCall();
         }
     }, 10000);
 }
